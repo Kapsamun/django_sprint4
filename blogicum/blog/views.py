e46204d218ed3bd9ac27aa8171c996a845b45fe4
Lina <alinakapsamun58@gmail.com>
2025-06-05 23:18:02 +0300

    Summary

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -127,20 +127,24 @@ class PostUpdateView(LoginRequiredMixin, UpdateView):
 
 class PostDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
-    template_name = 'blog/detail.html'
+    template_name = 'blog/post_confirm_delete.html'
     pk_url_kwarg = 'post_id'
-
+    
     def dispatch(self, request, *args, **kwargs):
         obj = self.get_object()
-        if obj.author != self.request.user:
+        if obj.author != request.user:
             return redirect('blog:post_detail', id=obj.pk)
         return super().dispatch(request, *args, **kwargs)
-
+    
     def get_success_url(self):
         return reverse_lazy(
             'blog:profile',
             kwargs={'username': self.request.user.username}
         )
+    
+    def delete(self, request, *args, **kwargs):
+        print(f"Удаление поста {self.get_object().id}")
+        return super().delete(request, *args, **kwargs)
 
 
 class ProfileUpdateView(LoginRequiredMixin, UpdateView):
